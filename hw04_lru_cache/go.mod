e902c75feb6434fd5149bc77e6e275d0206055da
slem7451 <slem7451@gmail.com>
2024-10-21 02:34:19 +0500

    add list

--- hw04_lru_cache/go.mod
+++ hw04_lru_cache/go.mod
@@ -1,6 +1,6 @@
-module github.com/fixme_my_friend/hw04_lru_cache
+module github.com/slem7451/home_work/hw04_lru_cache
 
-go 1.22
+go 1.23
 
 require github.com/stretchr/testify v1.7.0
 
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -27,6 +27,12 @@ func TestList(t *testing.T) {
 		l.Remove(middle)         // [10, 30]
 		require.Equal(t, 2, l.Len())
 
+		elems := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			elems = append(elems, i.Value.(int))
+		}
+		require.Equal(t, []int{10, 30}, elems)
+
 		for i, v := range [...]int{40, 50, 60, 70, 80} {
 			if i%2 == 0 {
 				l.PushFront(v)
@@ -42,10 +48,21 @@ func TestList(t *testing.T) {
 		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
 		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
 
-		elems := make([]int, 0, l.Len())
+		elems = make([]int, 0, l.Len())
 		for i := l.Front(); i != nil; i = i.Next {
 			elems = append(elems, i.Value.(int))
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+
+		count := 0
+		for i := l.Front(); i != nil; i = i.Next {
+			require.Equal(t, elems[count], i.Value)
+			count++
+			l.Remove(i)
+		}
+
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
 	})
 }
