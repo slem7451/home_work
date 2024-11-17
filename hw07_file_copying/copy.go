5a5545a37725c0faf61d32af3b0b5608aa296ebc
slem7451 <slem7451@gmail.com>
2024-11-17 14:53:52 +0500

    add copy

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -10,6 +14,48 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer fileFrom.Close()
+
+	fileFromStats, err := fileFrom.Stat()
+	if err != nil {
+		return err
+	}
+
+	size := fileFromStats.Size()
+
+	if size < 0 {
+		return ErrUnsupportedFile
+	}
+
+	if size < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 || limit > size - offset {
+		limit = size - offset
+	}
+
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fileTo.Close()
+
+	readSection := io.NewSectionReader(fileFrom, offset, limit)
+
+	bar := pb.New(int(limit))
+	bar.Start()
+	reader := bar.NewProxyReader(readSection)
+
+	_, err = io.CopyN(fileTo, reader, limit)
+	bar.Finish()
+	if err != nil && err != io.EOF {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,10 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/slem7451/home_work/hw07_file_copying
 
-go 1.22
+go 1.23
+
+require github.com/cheggaaa/pb v1.0.29
+
+require (
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
