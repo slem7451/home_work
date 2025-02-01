eb1c7eea3f332e690402966b016e139fe24ca105
slem7451 <slem7451@gmail.com>
2025-02-01 21:02:42 +0500

    add docker

--- hw12_13_14_15_calendar/internal/config/scheduler/config_test.go
+++ hw12_13_14_15_calendar/internal/config/scheduler/config_test.go
@@ -14,13 +14,13 @@ func TestConfig(t *testing.T) {
 
 	require.Equal(t, "INFO", config.Logger.Level)
 
-	require.Equal(t, "localhost", config.DB.Host)
+	require.Equal(t, "postgres", config.DB.Host)
 	require.Equal(t, 5432, config.DB.Port)
 	require.Equal(t, "root", config.DB.User)
 	require.Equal(t, "root", config.DB.Password)
 	require.Equal(t, "app_db", config.DB.Name)
 
-	require.Equal(t, "localhost", config.Rabbit.Host)
+	require.Equal(t, "rabbitmq", config.Rabbit.Host)
 	require.Equal(t, 5672, config.Rabbit.Port)
 	require.Equal(t, "guest", config.Rabbit.User)
 	require.Equal(t, "guest", config.Rabbit.Password)
